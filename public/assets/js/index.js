4ae87f67fa844897fc39221f002ed4af2938a5e8
NaokiSHIMAI <shimai@planningdev.com>
2020-10-21 17:46:05 +0900

    仮想ページ追加

---
+++ public/assets/js/index.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+  //スプレッドシートIDの指定と必要なJSONデータの取得
+  var data = "https://spreadsheets.google.com/feeds/list/1BdfhwOAZdRyEGMztCw8IjnYhGtOzBrNOeGlWL9eY8Oo/4/public/values?alt=json";
+  console.log(data);
+});
